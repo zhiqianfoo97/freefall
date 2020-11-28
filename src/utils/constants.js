e1f230b56f97d9d180a35c50f62fdfb2c034bb32
zwyeo97 <zwyeo97@hotmail.com>
2020-11-29 01:58:00 +0800

    add speed configuration at src/utils/constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,14 @@
 import {Dimensions} from 'react-native';
 const {height, width} = Dimensions.get('window');
 
-const Constants = {TOP_PIPE_WIDTH: 250,BOTTOM_PIPE_WIDTH: 100, HEIGHT: height, WIDTH: width, VELOCITY: -3, SNAKE_WIDTH: 25};
+const Constants = {
+    TOP_PIPE_WIDTH: 250,
+    BOTTOM_PIPE_WIDTH: 100, 
+    HEIGHT: height, 
+    WIDTH: width, 
+    SNAKE_VELOCITY: 3, //snake velocity always positive
+    OBJECT_VELOCITY: -10,  // object velocity always negative
+    SNAKE_WIDTH: 25,
+};
+
 export default Constants;
